38c02ab0ed0ca095f7fd0eb1550576f45b30d9dc
AnsPrac <bhattians258@gmail.com>
2023-08-30 04:33:47 +0500

    First commit

---
+++ src/FirstPackage/FirstClass.java
@@ -0,0 +1,17 @@
+package FirstPackage;
+
+public class FirstClass {
+	
+	
+	static void myMethod() {
+	    System.out.println("I just got executed!");
+	  }
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			 
+		myMethod();
+		
+		}
+
+}
