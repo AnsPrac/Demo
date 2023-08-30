883a71377daff113e931a5bfc55db93b4a430b6f
AnsPrac <bhattians258@gmail.com>
2023-08-30 23:49:49 +0500

    try_one

--- src/FirstPackage/FirstClass.java
+++ src/FirstPackage/FirstClass.java
@@ -3,14 +3,14 @@ package FirstPackage;
 public class FirstClass {
 	
 	
-	static void myMethod() {
+	static void myMethodd() {
 	    System.out.println("I just got executed!");
 	  }
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 			 
-		myMethod();
+		myMethodd();
 		
 		}
 
